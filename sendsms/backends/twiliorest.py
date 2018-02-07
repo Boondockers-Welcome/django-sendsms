3188b474ff992d1fa17916e506cc2dce97943e73
Anna Maste <skulegirlanna@gmail.com>
2018-02-07 13:41:50 -0500

    Add support for MESSAGING_SID

--- sendsms/backends/twiliorest.py
+++ sendsms/backends/twiliorest.py
@@ -3,29 +3,34 @@
 """
 this backend requires the twilio python library: http://pypi.python.org/pypi/twilio/
 """
-try:
-    from twilio.rest import TwilioRestClient
-except ImportError:
-    from twilio.rest import Client as TwilioRestClient
+from twilio.rest import Client
 
 from django.conf import settings
 from sendsms.backends.base import BaseSmsBackend
 
 TWILIO_ACCOUNT_SID = getattr(settings, 'SENDSMS_TWILIO_ACCOUNT_SID', '')
 TWILIO_AUTH_TOKEN = getattr(settings, 'SENDSMS_TWILIO_AUTH_TOKEN', '')
+TWILIO_MESSAGING_SID = getattr(settings, 'SENDSMS_TWILIO_MESSAGING_SID', '')
 
 
 class SmsBackend(BaseSmsBackend):
     def send_messages(self, messages):
-        client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
         for message in messages:
             for to in message.to:
                 try:
-                    client.messages.create(
-                        to=to,
-                        from_=message.from_phone,
-                        body=message.body
-                    )
+                    if TWILIO_MESSAGING_SID:
+                        client.messages.create(
+                            to=to,
+                            messaging_service_sid=TWILIO_MESSAGING_SID,
+                            body=message.body
+                        )
+                    else:
+                        client.messages.create(
+                            to=to,
+                            from_=message.from_phone,
+                            body=message.body
+                        )
                 except:
                     if not self.fail_silently:
                         raise
